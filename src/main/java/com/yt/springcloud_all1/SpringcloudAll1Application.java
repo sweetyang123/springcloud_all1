73bb270f89d9032eec0b35f0eb681f8cfc948d69
sweetyang123 <84315764+sweetyang123@users.noreply.github.com>
2021-09-12 17:12:51 +0800

    eureka

---
+++ src/main/java/com/yt/springcloud_all1/SpringcloudAll1Application.java
@@ -0,0 +1,13 @@
+package com.yt.springcloud_all1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAll1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAll1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yt/springcloud_all1/SpringcloudAll1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yt.springcloud_all1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAll1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
